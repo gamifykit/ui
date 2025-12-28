3d3f688ce8db73cb63b447ce7c952ec6afa494fd
Kaiden See (Github-verified) <457429+glsee@users.noreply.github.com>
2025-12-28 15:01:02 +0800

    chore: update posthog to use custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,20 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
+  async rewrites() {
+    return [
+      {
+        source: '/ph/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ph/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+    ]
+  },
   reactStrictMode: true,
+  skipTrailingSlashRedirect: true,
 };
 
 export default withMDX(config);
